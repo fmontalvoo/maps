f39d583568e2f86900c5bc57f2ae2727e5b4fbe6
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-03 17:21:02 -0500

    Agrego angular material.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
