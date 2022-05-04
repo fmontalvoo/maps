df14aac5138d83b8792c8fb9baa303219a476030
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-03 20:09:46 -0500

    Ahora se pueden agregar marcadores al mapa la hacer click.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 
+import { MarkerModel } from "src/app/models/marker.model";
+
 @Component({
   selector: "app-map",
   templateUrl: "./map.component.html",
   styleUrls: ["./map.component.css"],
 })
 export class MapComponent implements OnInit {
-  public lat: number = 51.678418;
-  public lng: number = 7.809007;
-  constructor() {}
+  public lat: number = -0.17239496915142513;
+  public lng: number = -78.48261026997494;
+
+  public markers: MarkerModel[] = [];
+
+  constructor() {
+    const newMarker = new MarkerModel(-0.17239496915142513, -78.48261026997494);
+    this.markers.push(newMarker);
+  }
 
   ngOnInit(): void {
+  }
+
+  public addMarker(event: any): void {
+    const { lat, lng } = event.coords;
+    console.info(lat, lng);
+    this.markers.push(new MarkerModel(lat, lng));
   }
 }
---
+++ src/app/models/marker.model.ts
@@ -0,0 +1,37 @@
+export class MarkerModel {
+  private _lat!: number;
+  private _lng!: number;
+  private _title: string = "Título";
+  private _description: string = "Descipción";
+
+  constructor(lat: number, lng: number) {
+    this._lat = lat;
+    this._lng = lng;
+  }
+
+  set lat(value: number) {
+    this._lat = value;
+  }
+  set lng(value: number) {
+    this._lng = value;
+  }
+  set title(value: string) {
+    this._title = value;
+  }
+  set description(value: string) {
+    this._description = value;
+  }
+
+  get lat() {
+    return this._lat;
+  }
+  get lng() {
+    return this._lng;
+  }
+  get title() {
+    return this._title;
+  }
+  get description() {
+    return this._description;
+  }
+}
