df69c130b033b5254a3a898b4e1f5dfa399eec62
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-04 00:49:14 -0500

    Se almacenan los marcadores del mapa en el local storage del navegador.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -14,8 +14,24 @@ export class MapComponent implements OnInit {
   public markers: MarkerModel[] = [];
 
   constructor() {
-    const newMarker = new MarkerModel(-0.17239496915142513, -78.48261026997494);
-    this.markers.push(newMarker);
+    const markersInStoreage = localStorage.getItem("markers");
+    if (markersInStoreage) {
+      const items = JSON.parse(markersInStoreage);
+      for (const item of items) {
+        console.log(item);
+        const {
+          _lat: lat,
+          _lng: lng,
+          _title: title,
+          _description: description,
+        } = item;
+        const marker = new MarkerModel(lat, lng);
+        marker.title = title;
+        marker.description = description;
+        console.log(marker);
+        this.markers.push(marker);
+      }
+    }
   }
 
   ngOnInit(): void {
@@ -23,7 +39,12 @@ export class MapComponent implements OnInit {
 
   public addMarker(event: any): void {
     const { lat, lng } = event.coords;
-    console.info(lat, lng);
     this.markers.push(new MarkerModel(lat, lng));
+    this.saveInStorage();
+  }
+
+  private saveInStorage() {
+    console.log(this.markers);
+    localStorage.setItem("markers", JSON.stringify(this.markers));
   }
 }
